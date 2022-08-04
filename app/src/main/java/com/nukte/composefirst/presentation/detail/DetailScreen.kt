15c72ce8fb7c1e0a2ea4016a78a8c890756551a4
FatihOZTURK <f@tih.fyi>
2022-08-04 17:37:07 +0300

    Fix compose navigation

--- app/src/main/java/com/nukte/composefirst/presentation/detail/DetailScreen.kt
+++ app/src/main/java/com/nukte/composefirst/presentation/detail/DetailScreen.kt
@@ -5,11 +5,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavController
+import androidx.room.util.TableInfo
 
 @Composable
-fun DetailScreen(navController: NavController){
+fun DetailScreen(navController: NavController, backStackEntry: NavBackStackEntry) {
+    val charId = backStackEntry.arguments?.getInt("charId") ?: -1
+    val viewModel: DetailViewModel = hiltViewModel()
+    viewModel.getCharacterbyId(charId)
     Column(modifier = Modifier.fillMaxSize()) {
-        Text(text = "Hello")
+        Text(text = "Hello $charId")
     }
 }
